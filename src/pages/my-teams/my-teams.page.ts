35b57353b9a4596be911d613ce074406c54b43b7
abhishekpandya24 <abhishek.pandya@xoriant.com>
2017-01-23 12:07:37 +0530

    temp list added to my-team page

--- src/pages/my-teams/my-teams.page.ts
+++ src/pages/my-teams/my-teams.page.ts
@@ -1,16 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { LoadingController, NavController, NavParams } from 'ionic-angular';
 
-import { TournamentsPage } from '../pages';
+import { TeamHomePage, TournamentsPage } from '../pages';
+import { EliteApi } from '../../shared/shared';
 
 @Component({
     templateUrl: 'my-teams.page.html'
 })
 
-export class MyTeamsPage{
-    constructor( private nav : NavController){}
+export class MyTeamsPage {
+    favorites = [
+        {
+            team: { id: 6182, name: 'HC Elite 7th', coach: 'Steve Michelotti' },
+            tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+            tournamentName: 'March Madness Tournament'
+        },
+        {
+            team: { id: 805, name: 'HC Elite', coach: 'Michelotti' },
+            tournamentId: '98c6857e-b0d1-4295-b89e-2d95a45437f2',
+            tournamentName: 'Holiday Hoops Challenge'
+        }
+    ];
+    constructor(private nav: NavController,
+        private navParams: NavParams,
+        private loadingController: LoadingController,
+        private eliteApi: EliteApi) { }
 
-    goToTournaments(){
+    goToTournaments() {
         this.nav.push(TournamentsPage);
-    }   
+    }
+    favoriteTapped($event, favorite) {
+        let loader = this.loadingController.create({
+            content: 'Getting data...',
+            spinner: 'dots'
+            //dismissOnPageChange: true
+        });
+        loader.present().then(() => {
+            this.eliteApi.getTournamentData(favorite.tournamentId)
+                .subscribe(t => this.nav.push(TeamHomePage, favorite.team));
+                loader.dismiss();
+        }
+        )
+    }
 }
